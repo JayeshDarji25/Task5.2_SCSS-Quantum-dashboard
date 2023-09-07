4666b67ae894457be7fa5be3e6d0ad6ea9115381
Jayesh Darji <jayesh.darji@zeuslearning.mumbai>
2023-09-07 09:57:13 +0530

    changes commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let items = document.getElementById("items");
 var itm = document.getElementsByClassName("itm");
 
 for(var i=0;i<itm.length;i++){
-    itm[i].addEventListener("click", function() {
+    itm[i].addEventListener("click", () =>  {
         let current = document.getElementsByClassName("active");
         if (current.length > 0) { 
             current[0].className = current[0].className.replace(" active", "");
